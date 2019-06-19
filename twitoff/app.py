9d6de8b43c3edc8f90f50b776f036e2e1cd422cb
Jeffrowetull <jeffrowetull@gmail.com>
2019-06-19 09:48:39 -0700

    added lots of files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from .models import DB
+from decouple import config
+
 
 def create_app():
     '''Create and configure an instance of the Flask application.'''
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Hello TwitOff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
 
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POAT':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+        return render_template('user.html', title=name,
+                               tweets=tweets, message=message)
     return app
---
+++ twitoff/twitter.py
@@ -0,0 +1,27 @@
+'''Retrieve tweets, embeddings, and persist in the database'''
+import tweepy
+from decouple import config
+from .models import DB, Tweet, User
+import basilica
+
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
+                                    config('TWITTER_CONSUMER_SECRET'))
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
+                              config('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER = tweepy.API(TWITTER_AUTH)
+
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
+def add_user(username):
+    '''Add a user and their tweets'''
+    twitter_user=TWITTER.get_user(username)
+    db_user = User(id=twitter_user.id, name=twitter_user.screen_name, newest_tweet_id=tweets[0].id)
+
+    for tweet in tweets:
+        embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+        db_tweet =Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
+        DB.session.add(db_tweet)
+        twitter_user.tweets.append(db_tweet)
+    
+    DB.session.add(db_user)
+    DB.session.commit()
