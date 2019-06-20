52b725e118b90ad8571fc54ae4236bed24e0ce3b
Jeffrowetull <jeffrowetull@gmail.com>
2019-06-20 15:19:41 -0700

    ADDED CHAGE TO DB URL

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from .models import DB, User
 from decouple import config
@@ -8,7 +9,7 @@ from .predict import predict_user
 def create_app():
     '''Create and configure an instance of the Flask application.'''
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['ENV'] = config('ENV')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
